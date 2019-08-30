2a6208c5c7ef7b5bb2f63ef06672714799c21c18
John Chambers <johncham81@gmail.com>
2019-08-30 17:28:57 +0100

    fetch printful variant attributes

--- lib/printful_api/sync_variant.rb
+++ lib/printful_api/sync_variant.rb
@@ -3,7 +3,7 @@ module PrintfulAPI
 	class SyncVariant < APIResource
 		include PrintfulAPI::APIOperations::Get
 
-    api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency
+    api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency, :price, :size, :image, :color, :color_code, :color_code2, :in_stock, :product_id
     
 		belongs_to :store_product
 
