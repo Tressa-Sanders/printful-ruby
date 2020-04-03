43b9efb6e73f44d60d2a7278d6bcd49bc1fc1a94
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-03 15:41:46 -0700

    Update sync_variant.rb

--- lib/printful_api/sync_variant.rb
+++ lib/printful_api/sync_variant.rb
@@ -6,7 +6,6 @@ module PrintfulAPI
                 api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency
     
 		belongs_to :store_product, :inverse_of => :sync_variants
-                accepts_nested_attributes_for :store_product
 
 		#def self.resource_path
 		# '/store/products/'
