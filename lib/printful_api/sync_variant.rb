c209030113f0753442b09cb69598ffeec37f965b
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-03 14:53:23 -0700

    Update sync_variant.rb

--- lib/printful_api/sync_variant.rb
+++ lib/printful_api/sync_variant.rb
@@ -1,7 +1,6 @@
 module PrintfulAPI
 
 	class SyncVariant < APIResource
-		include PrintfulAPI::APIOperations::List
 		include PrintfulAPI::APIOperations::Get
 
                 api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency, :price, :size, :image, :color, :color_code, :color_code2, :in_stock, :product_id
