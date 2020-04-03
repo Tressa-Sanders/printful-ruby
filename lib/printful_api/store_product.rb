9a56300711a35714707520401b87f16c90116aec
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-03 14:49:00 -0700

    Update store_product.rb

--- lib/printful_api/store_product.rb
+++ lib/printful_api/store_product.rb
@@ -9,35 +9,17 @@ module PrintfulAPI
 		has_many :files, class: 'PrintfulAPI::FileType'
 		has_many :options, class: 'PrintfulAPI::OptionType'
                 has_many :sync_variants, class: 'PrintfulAPI::SyncVariant'
-    
-		def sync_variants
-			@sync_variants ||= PrintfulAPI::SyncVariant.list( sync_product_id: self.id )
-			@sync_variants
-		end
 
 		def self.resource_path
-			'/store/products'
+		  '/store/products'
 		end
 
 		def load_data( data )
-
-			if data['product'].present?
-
-				super(data['product'])
-
-        
-				@sync_variants = data['sync_variants'].collect do |sync_variant_data|
-					variant = PrintfulAPI::SyncVariant.new.load_data( sync_variant_data )
-					variant.sync_product = self
-					variant
-				end
-
-			else
-
-				super( data )
-
-			end
-
+		   if data['product'].present?
+		     super(data['product'])
+		   else
+		     super( data )
+		   end
 		end
 
 
