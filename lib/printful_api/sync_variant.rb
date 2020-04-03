03461a4d06285d78c4abebaf88c92792b7b6a572
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-03 15:38:37 -0700

    Update sync_variant.rb

--- lib/printful_api/sync_variant.rb
+++ lib/printful_api/sync_variant.rb
@@ -3,21 +3,22 @@ module PrintfulAPI
 	class SyncVariant < APIResource
 		include PrintfulAPI::APIOperations::Get
 
-                api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency, :price, :size, :image, :color, :color_code, :color_code2, :in_stock, :product_id
+                api_attributes :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency
     
-		belongs_to :store_product
+		belongs_to :store_product, :inverse_of => :sync_variants
+                accepts_nested_attributes_for :store_product
 
-		def self.resource_path
-		 '/store/products/'
-		end
+		#def self.resource_path
+		# '/store/products/'
+		#end
 		
-		def load_data( data )
-		 if data['product'].present?
-		   super(data['product'])
-		 else
-		   super( data )
-		 end
-		end
+		#def load_data( data )
+		# if data['product'].present?
+		#   super(data['product'])
+		# else
+		#   super( data )
+		# end
+		#end
 		
 	end
 
