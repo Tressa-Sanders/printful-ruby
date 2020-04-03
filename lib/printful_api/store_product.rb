f10054c016ae8e13f17a149229b741f4d8f585dd
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-03 16:21:33 -0700

    Update store_product.rb

--- lib/printful_api/store_product.rb
+++ lib/printful_api/store_product.rb
@@ -4,7 +4,7 @@ module PrintfulAPI
 		include PrintfulAPI::APIOperations::List
 		include PrintfulAPI::APIOperations::Get
     
-                api_attributes :id, :external_id, :name, :variants, :synced, :thumbnail_url, sync_variant_attributes: { :id, :external_id, :sync_product_id, :name, :synced, :variant_id, :retail_price, :currency }
+                api_attributes :id, :external_id, :name, :variants, :synced, :thumbnail_url, :sync_variant_attributes
     
                 has_many :sync_variants, class: 'PrintfulAPI::SyncVariant', :inverse_of => :store_product
 
