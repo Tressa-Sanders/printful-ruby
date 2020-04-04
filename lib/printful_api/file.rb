a48c16e447aae4882bcf1dabfa072429865c7a78
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-04 09:11:12 -0700

    Update file.rb

--- lib/printful_api/file.rb
+++ lib/printful_api/file.rb
@@ -2,7 +2,6 @@ module PrintfulAPI
 
 	class File < APIResource
 		include PrintfulAPI::APIOperations::List
-		include PrintfulAPI::APIOperations::Create
 		include PrintfulAPI::APIOperations::Get
 
 		api_attributes :id, :type, :hash, :url, :filename, :mime_type, :size, :width, :height, :dpi, :status, :created, :thumbnail_url, :preview_url, :visible
