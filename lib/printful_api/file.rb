a6c4e153762362f6cada4a24eee7a9c9aee85cda
Tressa S <39808139+Tressa-Sanders@users.noreply.github.com>
2020-04-04 09:10:22 -0700

    Update file.rb

--- lib/printful_api/file.rb
+++ lib/printful_api/file.rb
@@ -8,8 +8,10 @@ module PrintfulAPI
 		api_attributes :id, :type, :hash, :url, :filename, :mime_type, :size, :width, :height, :dpi, :status, :created, :thumbnail_url, :preview_url, :visible
 
 
+		belongs_to :store_product, class: 'PrintfulAPI::StoreProduct', :inverse_of => :files
+
 		def self.resource_path
-			'/files'
+		 '/store/products'
 		end
 
 	end
